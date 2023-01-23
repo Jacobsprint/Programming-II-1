fa6febac21277073366876d6030e8d63f055e9a0
Jacobsprint <112713231+Jacobsprint@users.noreply.github.com>
2023-01-23 14:37:28 +0000

    Update

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print( "Hello, World")
+print( "Hello, World!")
+# This is a single line comment
+
+""" Arthimetic Operators
++ Add
+- Subtract 
+* Multiply
+/ Divide
+// Floor Divide (Divide and round down to nearest int or whole number)
+% Mod (Modulus/ Remainder from int division)
+** Pow (Exponent)
+"""
+
+""" Assignment Operators
+= Value Assignment (x = y)
++= Add to (x += y shorthand for x = x + y)
+-= Subtract to (x -= shorthand for x = x - y)
+*=
+/=
+//=
+%=
+**=
+"""
+
+"""Data Types
+int - Interger (Whole Number) 
+float - Floating-point number (number w/ a decimal)
+str - String (any text in quotes)
+bool - Boolean (either True or False only)
+chr - Character (single keyboard character, in single-quotes)
+"""
