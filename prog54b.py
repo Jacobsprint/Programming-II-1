e139da4df74fbd4168aca279c79b69338db50af3
Jacobsprint <112713231+Jacobsprint@users.noreply.github.com>
2023-01-23 14:45:24 +0000

    Update

---
+++ prog54b.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter Number 1: "))
+num2 = int(input("Enter Number 2: "))
+num3 = int(input("Enter Number 3: "))
+num4 = int(input("Enter Number 4: "))
+sum = num1 + num2 + num3 + num4
+average = sum / 4
+
+print("Sum =", sum)
+print("Average =", average)
