591382c9062100b2e53680134b1ed4c5a5a12f2c
Jacobsprint <112713231+Jacobsprint@users.noreply.github.com>
2023-02-01 14:20:18 +0000

    Update

--- lp5-10.py
+++ lp5-10.py
@@ -1,7 +1,7 @@
 num1 = int(input("Enter a Number:"))
 num2 = int(input("Enter a Second Number:"))
 
-if num1 < 0 and num2 < 0:
+if num1 < 0 or num2 < 0:
   print("Enter a Positive Number:" )
 while num2 > 0:
   temp = num1 % num2
