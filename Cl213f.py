138e295d548fa73677eca5834d66ba777610a8f4
Jacobsprint <112713231+Jacobsprint@users.noreply.github.com>
2023-03-14 13:45:34 +0000

    update

--- Cl213f.py
+++ Cl213f.py
@@ -10,12 +10,12 @@ class Cl213f:
         kwh = int(line.strip())
         if kwh < 2000:
           self.cost += .07 * kwh
-          kwh = kwh - 2000
-          if kwh > 2000 and kwh < 8000:
+         
+        if kwh > 2000 and kwh < 8000:
             self.cost += .05 * 8000
-            kwh = kwh - 8000
-            if kwh > 8000:
-              self.cost += kwh * .04
+            
+        if kwh > 8000:
+            self.cost += kwh * .04
 
   def __str__(self):
     return f"The Cost of {self.kwh} is ${str(self.cost)} "
