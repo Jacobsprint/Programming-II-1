fc9877e59df3eaab5052ed8bf574c7fa839b36a2
Jacobsprint <112713231+Jacobsprint@users.noreply.github.com>
2023-03-28 13:46:03 +0000

    Update

---
+++ prog464d.py
@@ -0,0 +1,23 @@
+import random
+import numpy as np
+
+
+def posMatrix(mat):
+  rows = len(mat1)
+  cols = len(mat1[0])
+  mOut = np.empty((rows, cols), dtype = 'int')
+
+
+def printMatrix(mat):
+  for row in mat:
+    for num in row:
+      print(f"{num:3d}" , end = "")
+    print()
+
+
+def main():
+  pass
+
+
+if __name__ == "__main__":
+  main() 
