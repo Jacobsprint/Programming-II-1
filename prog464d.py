b9106d20e74bb7832879d208efa22fb411969b52
Jacobsprint <112713231+Jacobsprint@users.noreply.github.com>
2023-03-30 01:49:42 +0000

    Update

--- prog464d.py
+++ prog464d.py
@@ -2,10 +2,14 @@ import random
 import numpy as np
 
 
-def posMatrix(mat):
-  rows = len(mat1)
-  cols = len(mat1[0])
-  mOut = np.empty((rows, cols), dtype = 'int')
+def transpose(mat):
+  rows = len(mat)
+  cols = len(mat[0])
+  mat2 = np.empty((rows, cols), dtype = 'int')
+  for r in range(5):
+    for c in range(5):
+      mat2[c][r] = mat[r][c]
+  return mat2 
 
 
 def printMatrix(mat):
@@ -16,7 +20,18 @@ def printMatrix(mat):
 
 
 def main():
-  pass
+  mat1 = []
+  for r in range(5):
+    row1 = []
+    for c in range(5):
+      rnd1 = random.randint(-50, 99)
+      row1.append(rnd1)
+    mat1.append(row1)
+  printMatrix(mat1)
+  print("\n")
+  mat2 = transpose(mat1) 
+  printMatrix(mat2)
+  
 
 
 if __name__ == "__main__":
